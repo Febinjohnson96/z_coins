765250421f7807ae77de9ba0a4791cc7d62f8a24
Febin Johnson <febinnj1996@gmail.com>
2023-12-20 11:15:31 +0530

    added pretty printer for logger

--- lib/utils/logger/app_logger.dart
+++ lib/utils/logger/app_logger.dart
@@ -1,23 +1,32 @@
 import 'package:logger/logger.dart';
 
-class AppLogger{
+class AppLogger {
   AppLogger._();
-  static final logger = Logger();
+  static final logger = Logger(
+      printer: PrettyPrinter(
+    methodCount: 0,
+    errorMethodCount: 0,
+    colors: true,
+    printTime: false,
+  ));
 
-    static debuglog(String message){
+  static debuglog(String message) {
     logger.d(message);
   }
 
-  static warninglog(String message){
+  static warninglog(String message) {
     logger.w(message);
   }
-  static errorlog(String message){
+
+  static errorlog(String message) {
     logger.e(message);
   }
-  static infolog(String message){
+
+  static infolog(String message) {
     logger.i(message);
   }
-  static fatalLog(String message){
+
+  static fatalLog(String message) {
     logger.f(message);
   }
 }
